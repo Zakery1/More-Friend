fe0a5cfc16eade0e9a221a6ecd236184f4a88920
Zachary Graham <zakgraham@gmail.com>
2019-01-06 11:26:10 -0700

    clean up routing

--- src/app/price-varied-dp/price-varied-dp.component.ts
+++ src/app/price-varied-dp/price-varied-dp.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-price-varied-dp',
-  templateUrl: './price-varied-dp.component.html',
-  styleUrls: ['./price-varied-dp.component.scss']
+	selector: 'app-price-varied-dp',
+	templateUrl: './price-varied-dp.component.html',
+	styleUrls: ['./price-varied-dp.component.scss']
 })
 export class PriceVariedDpComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
