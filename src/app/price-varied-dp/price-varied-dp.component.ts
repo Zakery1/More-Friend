d4dc83d0f3f063b316a37bd90803e71fdfd27902
Zachary Graham <zakgraham@gmail.com>
2019-01-26 11:51:36 -0700

    remove auto updates

--- src/app/price-varied-dp/price-varied-dp.component.ts
+++ src/app/price-varied-dp/price-varied-dp.component.ts
@@ -64,18 +64,47 @@ export class PriceVariedDpComponent implements OnInit {
 		this.dpPercentage = !this.dpPercentage;
 		if (this.dpPercentage === false) {
 			this.downPaymentOne = this.downPaymentTwo = this.downPaymentThree = this.downPaymentFour = this.downPaymentPercentage;
-			console.log(this.downPaymentOne, this.downPaymentTwo, this.downPaymentThree, this.downPaymentFour);
+			// console.log(this.downPaymentOne, this.downPaymentTwo, this.downPaymentThree, this.downPaymentFour);
 			this.loanAmountOne = (this.purchasePriceOne - this.downPaymentPercentage) * (100 + this.upfrontMiFf) / 100;
 			this.loanAmountTwo = (this.purchasePriceTwo - this.downPaymentPercentage) * (100 + this.upfrontMiFf) / 100;
 			this.loanAmountThree = (this.purchasePriceThree - this.downPaymentPercentage) * (100 + this.upfrontMiFf) / 100;
 			this.loanAmountFour = (this.purchasePriceFour - this.downPaymentPercentage) * (100 + this.upfrontMiFf) / 100;
 			console.log(this.loanAmountOne, this.loanAmountTwo, this.loanAmountThree, this.loanAmountFour);
+			// tslint:disable-next-line:max-line-length
+			this.pAndIOne = ((this.interestRate / 1200.0 * this.loanAmountOne) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			console.log(this.pAndIOne);
+			// tslint:disable-next-line:max-line-length
+			this.pAndITwo = ((this.interestRate / 1200.0 * this.loanAmountTwo) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			// tslint:disable-next-line:max-line-length
+			this.pAndIThree = ((this.interestRate / 1200.0 * this.loanAmountThree) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			// tslint:disable-next-line:max-line-length
+			this.pAndIFour = ((this.interestRate / 1200.0 * this.loanAmountFour) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			this.mortgageInsuranceOne = ((this.purchasePriceOne - this.downPaymentOne) * this.miPercentage / 1200).toFixed(2);
+			this.mortgageInsuranceTwo = ((this.purchasePriceTwo - this.downPaymentTwo) * this.miPercentage / 1200).toFixed(2);
+			this.mortgageInsuranceThree = ((this.purchasePriceThree - this.downPaymentThree) * this.miPercentage / 1200).toFixed(2);
+			this.mortgageInsuranceFour = ((this.purchasePriceFour - this.downPaymentFour) * this.miPercentage / 1200).toFixed(2);
 
 		} else {
 			this.downPaymentOne = (this.purchasePriceOne * this.downPaymentPercentage) / 100;
 			this.downPaymentTwo = (this.purchasePriceTwo * this.downPaymentPercentage) / 100;
 			this.downPaymentThree = (this.purchasePriceThree * this.downPaymentPercentage) / 100;
 			this.downPaymentFour = (this.purchasePriceFour * this.downPaymentPercentage) / 100;
+			this.loanAmountOne = (this.purchasePriceOne * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100;
+			this.loanAmountTwo = (this.purchasePriceTwo * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100;
+			this.loanAmountThree = (this.purchasePriceThree * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100;
+			this.loanAmountFour = (this.purchasePriceFour * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100;
+			// tslint:disable-next-line:max-line-length
+			this.pAndIOne = ((this.interestRate / 1200.0 * this.loanAmountOne) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			// tslint:disable-next-line:max-line-length
+			this.pAndITwo = ((this.interestRate / 1200.0 * this.loanAmountTwo) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			// tslint:disable-next-line:max-line-length
+			this.pAndIThree = ((this.interestRate / 1200.0 * this.loanAmountThree) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			// tslint:disable-next-line:max-line-length
+			this.pAndIFour = ((this.interestRate / 1200.0 * this.loanAmountFour) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+			this.mortgageInsuranceOne = ((this.purchasePriceOne - this.downPaymentOne) * this.miPercentage / 1200).toFixed(2);
+			this.mortgageInsuranceTwo = ((this.purchasePriceTwo - this.downPaymentTwo) * this.miPercentage / 1200).toFixed(2);
+			this.mortgageInsuranceThree = ((this.purchasePriceThree - this.downPaymentThree) * this.miPercentage / 1200).toFixed(2);
+			this.mortgageInsuranceFour = ((this.purchasePriceFour - this.downPaymentFour) * this.miPercentage / 1200).toFixed(2);
 		}
 	}
 
