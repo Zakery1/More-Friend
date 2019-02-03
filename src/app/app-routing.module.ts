c60126073406af075d27e746028e2f237e01d4ff
Zachary Graham <zakgraham@gmail.com>
2019-02-03 14:54:44 -0700

    set up routing for One Prop Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PriceVariedDpComponent } from './price-varied-dp/price-varied-dp.component';
 import { PriceVariedExactDpComponent } from './price-varied-exact-dp/price-varied-exact-dp.component';
+import { PriceOnePropDpComponent } from './price-one-prop-dp/price-one-prop-dp.component';
+import { PriceOnePropExactComponent } from './price-one-prop-exact/price-one-prop-exact.component';
 
 const routes: Routes = [
 	{ path: 'pvdp', component: PriceVariedDpComponent },
 	{ path: 'pvdpD', component: PriceVariedExactDpComponent },
+	{ path: 'pvdpD', component: PriceVariedExactDpComponent },
+	{ path: 'podp', component: PriceOnePropDpComponent },
+	{ path: 'podpE', component: PriceOnePropExactComponent},
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
 
--- src/app/price-one-prop-exact/price-one-prop-exact.component.ts
+++ src/app/price-one-prop-exact/price-one-prop-exact.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-price-one-prop-exact',
-  templateUrl: './price-one-prop-exact.component.html',
-  styleUrls: ['./price-one-prop-exact.component.scss']
+	selector: 'app-price-one-prop-exact',
+	templateUrl: './price-one-prop-exact.component.html',
+	styleUrls: ['./price-one-prop-exact.component.scss']
 })
 export class PriceOnePropExactComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
