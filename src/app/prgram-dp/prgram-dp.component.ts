6dac04b35620d10d859e110008cf69167e5dc18d
Zachary Graham <zakgraham@gmail.com>
2019-02-06 20:49:39 -0700

    generate programdp component

---
+++ src/app/prgram-dp/prgram-dp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prgram-dp',
+  templateUrl: './prgram-dp.component.html',
+  styleUrls: ['./prgram-dp.component.scss']
+})
+export class PrgramDPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
