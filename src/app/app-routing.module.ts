ce020027811d824fc1aee5366d272940179c8ae4
Zachary Graham <zakgraham@gmail.com>
2019-02-07 20:30:00 -0700

    begin functionality on program component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProgramDpComponent } from './modules/situations/components/program-dp/program-dp.component';
 import { PriceOnePropExactComponent } from './modules/situations/components/price-one-prop-exact/price-one-prop-exact.component';
 import { PriceOnePropDpComponent } from './modules/situations/components/price-one-prop-dp/price-one-prop-dp.component';
 import { PriceVariedExactDpComponent } from './modules/situations/components/price-varied-exact-dp/price-varied-exact-dp.component';
 import { PriceVariedDpComponent } from './modules/situations/components/price-varied-dp/price-varied-dp.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{ path: 'pvdp', component: PriceVariedDpComponent },
 	{ path: 'pvdpD', component: PriceVariedExactDpComponent },
-	{ path: 'pvdpD', component: PriceVariedExactDpComponent },
+	{ path: 'programDP', component: ProgramDpComponent },
 	{ path: 'podp', component: PriceOnePropDpComponent },
 	{ path: 'podpE', component: PriceOnePropExactComponent},
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
--- src/app/modules/situations/components/program-dp/program-dp.component.ts
+++ src/app/modules/situations/components/program-dp/program-dp.component.ts
@@ -1,15 +1,121 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-program-dp',
-  templateUrl: './program-dp.component.html',
-  styleUrls: ['./program-dp.component.scss']
+	selector: 'app-program-dp',
+	templateUrl: './program-dp.component.html',
+	styleUrls: ['./program-dp.component.scss']
 })
 export class ProgramDpComponent implements OnInit {
 
-  constructor() { }
+	private purchasePrice = 340000;
 
-  ngOnInit() {
-  }
+	private downPaymentPercentage = 3.5;
+	private upfrontMiFf = 0;
+	private miPercentage = 0;
+	private estimatedTaxes = 72;
+	private estimateHOI = 61.5;
+	private interestRate = 3.75;
+	private mortgageYears = 15;
+
+	private downPaymentOne = (this.purchasePrice * this.downPaymentPercentage) / 100;
+	private downPaymentTwo = (this.purchasePrice * this.downPaymentPercentage) / 100;
+	private downPaymentThree = (this.purchasePrice * this.downPaymentPercentage) / 100;
+	private downPaymentFour = (this.purchasePrice * this.downPaymentPercentage) / 100;
+
+	private loanAmountOne = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+	private loanAmountTwo = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+		// tslint:disable-next-line:max-line-length
+	private loanAmountThree = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+	private loanAmountFour = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100);
+
+	// tslint:disable-next-line:max-line-length
+	private pAndIOne = ((this.interestRate / 1200.0 * +this.loanAmountOne) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+	// tslint:disable-next-line:max-line-length
+	private pAndITwo = ((this.interestRate / 1200.0 * +this.loanAmountTwo) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+	// tslint:disable-next-line:max-line-length
+	private pAndIThree = ((this.interestRate / 1200.0 * +this.loanAmountThree) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+	// tslint:disable-next-line:max-line-length
+	private pAndIFour = ((this.interestRate / 1200.0 * +this.loanAmountFour) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+
+	private mortgageInsuranceOne = +((this.purchasePrice - this.downPaymentOne) * this.miPercentage / 1200).toFixed(2);
+	private mortgageInsuranceTwo = +((this.purchasePrice - this.downPaymentTwo) * this.miPercentage / 1200).toFixed(2);
+	private mortgageInsuranceThree = +((this.purchasePrice - this.downPaymentThree) * this.miPercentage / 1200).toFixed(2);
+	private mortgageInsuranceFour = +((this.purchasePrice - this.downPaymentFour) * this.miPercentage / 1200).toFixed(2);
+
+	private estimatedPTOne = this.estimatedTaxes;
+	private estimatedPTTwo = this.estimatedTaxes;
+	private estimatedPTThree = this.estimatedTaxes;
+	private estimatedPTFour = this.estimatedTaxes;
+
+	private estimatedHIOne = this.estimateHOI;
+	private estimatedHITwo = this.estimateHOI;
+	private estimatedHIThree = this.estimateHOI;
+	private estimatedHIFour = this.estimateHOI;
+
+	private estimatedPaymentOne = (+this.pAndIOne + +this.mortgageInsuranceOne + +this.estimatedPTOne + +this.estimatedHIOne).toFixed(2);
+	private estimatedPaymentTwo = (+this.pAndITwo + +this.mortgageInsuranceTwo + +this.estimatedPTTwo + +this.estimatedHITwo).toFixed(2);
+		// tslint:disable-next-line:max-line-length
+	private estimatedPaymentThree = (+this.pAndIThree + +this.mortgageInsuranceThree + +this.estimatedPTThree + +this.estimatedHIThree).toFixed(2);
+	private estimatedPaymentFour = (+this.pAndIFour + +this.mortgageInsuranceFour + +this.estimatedPTFour + +this.estimatedHIFour).toFixed(2);
+
+	private update() {
+		this.downPaymentOne = this.downPaymentOne;
+		this.downPaymentTwo = this.downPaymentTwo;
+		this.downPaymentThree = this.downPaymentThree;
+		this.downPaymentFour = this.downPaymentFour;
+			// tslint:disable-next-line:max-line-length
+
+		this.loanAmountOne = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+		this.loanAmountTwo = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+		// tslint:disable-next-line:max-line-length
+		this.loanAmountThree = ((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+		this.loanAmountFour = +((this.purchasePrice * (100 - this.downPaymentPercentage) / 100) * (100 + this.upfrontMiFf) / 100).toFixed(2);
+
+		// tslint:disable-next-line:max-line-length
+		this.pAndIOne = ((this.interestRate / 1200.0 * +this.loanAmountOne) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+		// tslint:disable-next-line:max-line-length
+		this.pAndITwo = ((this.interestRate / 1200.0 * +this.loanAmountTwo) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+		// tslint:disable-next-line:max-line-length
+		this.pAndIThree = ((this.interestRate / 1200.0 * +this.loanAmountThree) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+		// tslint:disable-next-line:max-line-length
+		this.pAndIFour = ((this.interestRate / 1200.0 * this.loanAmountFour) / (1.0 - Math.pow(1.0 + this.interestRate / 1200.00, -1.0 * this.mortgageYears * 12))).toFixed(2);
+
+		this.mortgageInsuranceOne = +((this.purchasePrice - this.downPaymentOne) * this.miPercentage / 1200).toFixed(2);
+		this.mortgageInsuranceTwo = +((this.purchasePrice - this.downPaymentTwo) * this.miPercentage / 1200).toFixed(2);
+		this.mortgageInsuranceThree = +((this.purchasePrice - this.downPaymentThree) * this.miPercentage / 1200).toFixed(2);
+		this.mortgageInsuranceFour = +((this.purchasePrice - this.downPaymentFour) * this.miPercentage / 1200).toFixed(2);
+
+		this.estimatedPTOne = this.estimatedTaxes;
+		this.estimatedPTTwo = this.estimatedTaxes;
+		this.estimatedPTThree = this.estimatedTaxes;
+
+		this.estimatedHIOne = this.estimateHOI;
+		this.estimatedHITwo = this.estimateHOI;
+		this.estimatedHIThree = this.estimateHOI;
+		this.estimatedHIFour = this.estimateHOI;
+
+		this.estimatedPaymentOne = (+this.pAndIOne + +this.mortgageInsuranceOne + +this.estimatedPTOne + +this.estimatedHIOne).toFixed(2);
+		this.estimatedPaymentTwo = (+this.pAndITwo + +this.mortgageInsuranceTwo + +this.estimatedPTTwo + +this.estimatedHITwo).toFixed(2);
+			// tslint:disable-next-line:max-line-length
+		this.estimatedPaymentThree = (+this.pAndIThree + +this.mortgageInsuranceThree + +this.estimatedPTThree + +this.estimatedHIThree).toFixed(2);
+		this.estimatedPaymentFour = (+this.pAndIFour + +this.mortgageInsuranceFour + +this.estimatedPTFour + +this.estimatedHIFour).toFixed(2);
+	}
+
+	private toOriginal() {
+		this.purchasePrice = 340000;
+		this.downPaymentPercentage = 5;
+		this.upfrontMiFf = 1.75;
+		this.miPercentage = 0.85;
+		this.estimatedTaxes = 0.8;
+		this.estimateHOI = 0.35;
+		this.interestRate = 4.125;
+		this.mortgageYears = 30;
+		this.update();
+	}
+
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
