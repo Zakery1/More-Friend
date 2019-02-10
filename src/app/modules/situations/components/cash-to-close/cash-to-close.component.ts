e7117b4c7f7403095ee9728b4b93f927cdf42439
Zachary Graham <zakgraham@gmail.com>
2019-02-10 03:31:28 -0700

    add values to html

--- src/app/modules/situations/components/cash-to-close/cash-to-close.component.ts
+++ src/app/modules/situations/components/cash-to-close/cash-to-close.component.ts
@@ -9,21 +9,29 @@ export class CashToCloseComponent implements OnInit {
 
 	private downPayment = 6615;
 	private originationFC = 3712;
-	private earnestMoney = 1000;
-	private sellerCredit = 5670;
+	private earnestMoney = -1000;
+	private sellerCredit = -5670;
 	private otherDPA = -7265;
 	private dailyInterest = 396;
 	private oneYearHI = 360000;
 	private prepaidITmE = 602;
 	private titleFees = 1650;
-	private countryRecordingFees = 40;
+	private countyRecordingFees = 40;
+	private homeWarranty = 500;
 	private prepaidHOADues = 380;
 	private hOATransferFees = 200;
 	private dPAAdminFee = 1145;
+	private underwritingFee = 0;
 
-	constructor() { }
 
-	ngOnInit() {
-	}
+	private totalEstimatedClosingCosts = (this.underwritingFee + this.dailyInterest + 1)
+
+		// private totalEstimatedCashToClose = (this.downPayment + closingC)
+
+
+		constructor() { }
+
+ngOnInit() {
+}
 
 }
