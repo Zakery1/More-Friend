05496c58d5ce130d400073891b5d18e5622d09b6
Zachary Graham <zakgraham@gmail.com>
2019-02-09 19:11:59 -0700

    add cash to close declarations

--- src/app/modules/situations/components/cash-to-close/cash-to-close.component.ts
+++ src/app/modules/situations/components/cash-to-close/cash-to-close.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashToCloseComponent implements OnInit {
 
+	private downPayment = 6615;
+	private originationFC = 3712;
+	private earnestMoney = 1000;
+	private sellerCredit = 5670;
+	private otherDPA = -7265;
+	private dailyInterest = 396;
+	private oneYearHI = 360000;
+	private prepaidITmE = 602;
+	private titleFees = 1650;
+	private countryRecordingFees = 40;
+	private prepaidHOADues = 380;
+	private hOATransferFees = 200;
+	private dPAAdminFee = 1145;
+
 	constructor() { }
 
 	ngOnInit() {
